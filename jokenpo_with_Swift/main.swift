6028b9e9b0683d1d10204386e2751d6334d16276
thedevbr <thedevbr@gmail.com>
2021-10-25 00:57:18 -0300

    jokenpo_with_swift

--- jokenpo_with_Swift/main.swift
+++ jokenpo_with_Swift/main.swift
@@ -1,9 +1,9 @@
 let jokenpo = ["Pedra", "Papel", "Tesoura",""]
-var player1 = jokenpo[3]
-var player2 = jokenpo[3]
+var player1 = jokenpo[2]
+var player2 = jokenpo[1]
 
 if (player1 == jokenpo[3] || player2 == jokenpo[3]){
-  print("Por favor, escolha entre 'Pedra', 'Papel' ou 'Tesoura' ! ")
+  print("Por favor player, escolha entre 'Pedra', 'Papel' ou 'Tesoura' ! ")
 }else if( player1 == player2 ){
 
   print("Empate!")
@@ -12,6 +12,12 @@ if (player1 == jokenpo[3] || player2 == jokenpo[3]){
 
   print("Player 1 jogou   \(jokenpo[0]).")
   print("Player 2 jogou  \(jokenpo[1]).")
+  print("Player 2 Venceu !")
+
+}else if( player1 == jokenpo[1] && player2 == jokenpo[0]){
+
+  print("Player 1 jogou   \(jokenpo[1]).")
+  print("Player 2 jogou  \(jokenpo[0]).")
   print("Player 1 Venceu !")
 
 }else if( player1 == jokenpo[0] && player2 == jokenpo[2]){
