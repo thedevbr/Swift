8ffc041f281916e1aae03dfa920c80307d8cf014
thedevbr <thedevbr@gmail.com>
2021-10-24 23:40:26 -0300

    Jogo jokenpo criado com swift

---
+++ jokenpo_with_Swift/main.swift
@@ -0,0 +1,35 @@
+let jokenpo = ["Pedra", "Papel", "Tesoura",""]
+var player1 = jokenpo[3]
+var player2 = jokenpo[3]
+
+if (player1 == jokenpo[3] || player2 == jokenpo[3]){
+  print("Por favor, escolha entre 'Pedra', 'Papel' ou 'Tesoura' ! ")
+}else if( player1 == player2 ){
+
+  print("Empate!")
+
+}else if( player1 == jokenpo[0] && player2 == jokenpo[1]){
+
+  print("Player 1 jogou   \(jokenpo[0]).")
+  print("Player 2 jogou  \(jokenpo[1]).")
+  print("Player 1 Venceu !")
+
+}else if( player1 == jokenpo[0] && player2 == jokenpo[2]){
+
+  print("Player 1 jogou  \(jokenpo[0]).")
+  print("Player 2 jogou  \(jokenpo[2]).")
+  print("Player 1 Venceu !")
+
+}else if( player1 == jokenpo[1] && player2 == jokenpo[2]){
+
+  print("Player 1 jogou  \(jokenpo[1]).")
+  print("Player 2 jogou  \(jokenpo[2]).")
+  print("Player 2 Venceu !")
+
+}else if( player1 == jokenpo[2] && player2 == jokenpo[1]){
+
+  print("Player 1 jogou  \(jokenpo[2]).")
+  print("Player 2 jogou  \(jokenpo[1]).")
+  print("Player 1 Venceu !")
+
+}
