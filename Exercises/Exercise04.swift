a7ad72cb1dc6788e8ac5bd3e220390ec5f2b4556
Lucas Mateus <85304236+thedevbr@users.noreply.github.com>
2021-10-26 02:44:35 +0000

    adjusted

--- Exercises/Exercise04.swift
+++ Exercises/Exercise04.swift
@@ -2,13 +2,9 @@
  04) Write a Swift program to accept two integer values and return true if one is negative and one is positive. Return true only if both are negative.
 */
 
-var valueOne : Int = 10
-var valueTwo : Int = -10
-if(valueOne + valueTwo < 0){
-  print("...")
-}else if(valueOne + valueTwo == 0){
-  print("...")
-}else if(valueOne < 0) && (valueTwo > 0) ||
+var valueOne : Int = -10
+var valueTwo : Int = 10
+if(valueOne < 0) && (valueTwo > 0) ||
   (valueOne > 0) && (valueTwo < 0) || 
   (valueOne < 0) && (valueTwo < 0){
   print(true)
